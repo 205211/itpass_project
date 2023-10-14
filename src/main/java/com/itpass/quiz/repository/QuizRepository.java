8ba2ff34f5b988dd7c1852578863a36fda9f59bd
205211 <138965635+205211@users.noreply.github.com>
2023-10-14 06:37:15 +0000

    解答機能の改修

--- src/main/java/com/itpass/quiz/repository/QuizRepository.java
+++ src/main/java/com/itpass/quiz/repository/QuizRepository.java
@@ -1,5 +1,6 @@
 package com.itpass.quiz.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.itpass.quiz.entity.Quiz;
